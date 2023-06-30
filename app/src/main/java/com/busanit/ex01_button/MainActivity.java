ffc0a70b0a2cdc08b6920acc40ac92526fe99f19
dy6379 <dy637988@gmail.com>
2023-06-30 09:33:09 +0900

    first commit

---
+++ app/src/main/java/com/busanit/ex01_button/MainActivity.java
@@ -0,0 +1,14 @@
+package com.busanit.ex01_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
